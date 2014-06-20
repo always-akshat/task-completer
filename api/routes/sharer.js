99c8fd1a0aaa180bfbca0eb76bfa7fe2f907a5e7
akshat <always.akshat@gmail.com>
2014-06-20 16:33:45 +0530

    added facebook photo post

--- api/routes/sharer.js
+++ api/routes/sharer.js
@@ -11,7 +11,10 @@ exports.feed_post = function(req,res){
     console.log(req.session.student.facebook.authcode);
     FB.setAccessToken(req.session.student.facebook.authcode);
     var body = 'My first post using facebook-node-sdk';
-    FB.api('me/feed', 'post', { message: body}, function (res) {
+    FB.api('me/photos', 'post', { message: "Photo post theough node-facebook-sdk",
+                                url :'https://s3-ap-southeast-1.amazonaws.com/letsintern.com/viber/images/new_hp/image_1.jpg'
+                                                                }
+                        , function (res) {
         if(!res || res.error) {
             console.log(!res ? 'error occurred' : res.error);
             return;
