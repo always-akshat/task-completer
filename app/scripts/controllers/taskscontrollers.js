cdedce07e1ccd6c0071277ed21c3c6fb204ad45b
vaibhav <vaibhav.devpura@gmail.com>
2014-07-03 19:45:00 +0530

    integrated all sub pages + improved twitter implementation.

--- app/scripts/controllers/taskscontrollers.js
+++ app/scripts/controllers/taskscontrollers.js
@@ -27,18 +27,41 @@ viberApp.controller('vbInsertLinksCtrl',function($scope,$http,toaster,$q,postlin
     $scope.link = "https://www.youtube.com/watch?v=EOl4e8wsvHU";
     console.log($scope.identity.currentUser);
     var user_tasks = $scope.identity.currentUser.user_tasks;
-
-
-   var task = _.where(user_tasks,{'task_id':'53a9515ae4b041d6a3190435'})[0];
+    var task = _.where(user_tasks,{'task_id':'53a9515ae4b041d6a3190435'})[0];
 
     var fbsuccess=false, twsuccess=false;
-//    $scope.answers = task.answers;
+
+    var bindCtrl = function () {
+        $scope.answers = task.answers;
+
+        _.each($scope.answers,function(answer){
+
+            if(angular.isObject(answer.facebook)){
+
+                if(angular.isObject(answer.twitter)) {
+
+                    answer['fbsuccess'] = true;
+                    answer['twsuccess'] = true;
+                }
+                else {
+                    answer['fbsuccess'] = true;
+                    answer['twsuccess'] = false;
+                }
+
+            }
+            else{ answer['twsuccess'] = true;
+                answer['fbsuccess'] =false;
+            }
+        });
+    };
+
+    bindCtrl();
 
     if (angular.isObject($scope.identity.currentUser.facebook) && $scope.identity.currentUser.facebook.authorized == '1') $scope.checkedfb = true;
     if (angular.isObject($scope.identity.currentUser.facebook) && $scope.identity.currentUser.twitter.authorized == '1') $scope.checkedtw = true;
+    console.log(angular.isObject($scope.answers.facebook));
 
-
-        $scope.submitForm = function (isValid) {
+    $scope.submitForm = function (isValid) {
 
             if (isValid) {
 
@@ -51,6 +74,8 @@ viberApp.controller('vbInsertLinksCtrl',function($scope,$http,toaster,$q,postlin
                             twsuccess=true;
                             fbsuccess=true;
 
+                            bindCtrl();
+                            console.log("Tasks after share"+task.answers);
                         }
                         else {
 
@@ -63,6 +88,7 @@ viberApp.controller('vbInsertLinksCtrl',function($scope,$http,toaster,$q,postlin
 
                             toaster.pop('success', "Facebook Post", "Your Message has been posted successfully to Facebook");
                             fbsuccess=true;
+                            bindCtrl();
                         }
                         else {
 
@@ -75,7 +101,7 @@ viberApp.controller('vbInsertLinksCtrl',function($scope,$http,toaster,$q,postlin
                         if (success) {
                             toaster.pop('success', "Twitter Post", "Your Message has been posted successfully");
                             twsuccess=true;
-
+                            bindCtrl();
                         }
                         else {
 
@@ -84,48 +110,6 @@ viberApp.controller('vbInsertLinksCtrl',function($scope,$http,toaster,$q,postlin
                         }
                     }
                 });
-                /* if ($scope.checkedfb) {
-
-                    postlink.postfblink($scope.link, $scope.message,task).then(function (success) {
-
-                        if (success) {
-
-                            toaster.pop('success', "Facebook Post", "Your Message has been posted successfully to Facebook");
-                     }
-                        else {
-
-                            toaster.pop('failure', "Facebook Post", "There was an error in publishing your post");
-
-                        }
-
-
-                    });
-                }
-
-                else if ($scope.checkedtw) {
-
-                    postlink.posttwlink($scope.link, $scope.message,task).then(function (success) {
-
-                        if (success) {
-                            toaster.pop('success', "Twitter Post", "Your Message has been posted successfully");
-                            twsuccess=true;
-
-                        }
-                        else {
-
-                            toaster.pop('failure', "Twitter Post", "There was an error in publishing your post");
-
-                        }
-
-
-                    });
-                }
-
-                else if($scope.checkedtw && $scope.checkedfb){
-
-                }  */
-
-
 
             }
 
