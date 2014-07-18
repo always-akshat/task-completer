874a11218f0e0acb80e14cd7ce55fc40c8bcd819
akshat <always.akshat@gmail.com>
2014-07-18 21:35:09 +0530

    db change

--- api/config.js
+++ api/config.js
@@ -3,8 +3,8 @@
  */
 
 var mongoose = require('mongoose');
-//mongoose.connect('mongodb://viber:viber@ds041218.mongolab.com:41218/viber');
-mongoose.connect('mongodb://viber_app:akshat@ds053449.mongolab.com:53449/viber_prod');
+mongoose.connect('mongodb://viber:viber@ds041218.mongolab.com:41218/viber');
+//mongoose.connect('mongodb://viber_app:akshat@ds053449.mongolab.com:53449/viber_prod');
 
 exports.utils = require('../api/utils.js');
 
